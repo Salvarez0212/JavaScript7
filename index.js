bd0abae7943531b8fbbe799a2e0f4d594e51e0ca
Salvarez19 <alvarezsebastian31@gmail.com>
2023-02-12 11:07:37 -0500

    Corrección de la función

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 //Escribir una función que tome un objeto como argumento. El objeto contiene dos propiedades, upvotes y downvotes. Devuelve el número de votos a favor menos el número de votos en contra.
 
-function getVoteCount(obj) {
-  let votes = Object.values(obj);
-  return votes.reduce((a,b)=> a-b);
-};
-
-
-console.log(getVoteCount({upvotes:13, downvotes:0}))	//13
-console.log(getVoteCount({upvotes:2, downvotes:33}))	//-31
-console.log(getVoteCount({upvotes:132, downvotes:132}))	//0
+  function getVoteCount(obj) {
+    let votes = obj.upvotes - obj.downvotes;
+    return votes;
+  };
+  
+  
+  console.log(getVoteCount({upvotes:13, downvotes:0}))	//13
+  console.log(getVoteCount({upvotes:2, downvotes:33}))	//-31
+  console.log(getVoteCount({upvotes:132, downvotes:132}))	//0
+  console.log(getVoteCount({downvotes:13, upvotes:0})) //-13
